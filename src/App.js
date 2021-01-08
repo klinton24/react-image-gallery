4467fbbfc3bc1834f8c3041735bee67ff0e62cc0
K Smith <klinton.jp@gmail.com>
2021-01-08 16:02:08 -0700

    My First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Grid } from "@material-ui/core";
+import "./styles.css";
+import PplCard from "./components/PplCard";
 
-function App() {
+export default function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      setUsers(
+        await fetch("https://reqres.in/api/users?page=2")
+          .then(response => response.json())
+          .then(response => response.data)
+          .catch(err => console.log(err, "Fetch Warning"))
+      );
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Employee Database</h1>
+      <Grid container spacing={10}>
+        {users.map(user => (
+          <Grid key={user.id} item xs={12} sm={6} md={4} lg={4} xl={3}>
+            <PplCard
+              email={user.email}
+              fName={user.first_name}
+              lName={user.last_name}
+              picture={user.avatar}
+              key={user.id}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 }
-
-export default App;
+/*RnKSA-51kpI*/
